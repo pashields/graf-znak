e444321ccbebc1f24ae04385cc53b97884fb4673
Pat Shields <yeoldefortran@gmail.com>
2014-01-31 16:53:09 -0500

    Update project-clj version/description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject graf-znak "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject graf-znak "0.0.1-SNAPSHOT"
+  :description "A library for aggregating collections maps across multiple keys."
+  :url "http://github.com/pashields/graf-znak"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
