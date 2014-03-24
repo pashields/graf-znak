7626ce80b75471be681f075ac1a3762cc6873793
Pat Shields <yeoldefortran@gmail.com>
2014-03-24 19:59:35 -0400

    Update benchmarking script to new API.
    Still not super useful because the datasets are so small.

--- bench/bench.clj
+++ bench/bench.clj
@@ -2,27 +2,33 @@
   (:require [graf-znak.core :refer :all]
             [criterium.core :refer :all]
             [graf-znak.atom-storage :as atom-store]
-            [graf-znak.concurrent-hash-storage :as chash-store]))
+            [graf-znak.concurrent-hash-storage :as chash-store]
+            [graf-znak.accumulators :as accums]
+            [graf-znak.hooks :as hooks]))
 
 (defn- run
-  [factory map-fn inputs groups]
+  [factory map-fn inputs hooks]
   (bench
-   (let [net (create-net groups factory)]
+   (let [net (create-net hooks factory)]
      (doall
-      (map-fn (partial send-net net) inputs)))))
+      (map-fn (partial put net) inputs)))))
 
 (defn run-atom-single
   [inputs groups]
-  (run atom-store/factory map inputs groups))
+  (run atom-store/factory map inputs
+       [(hooks/->Hook groups [accums/counter])]))
 
 (defn run-atom-parallel
   [inputs groups]
-  (run atom-store/factory pmap inputs groups))
+  (run atom-store/factory pmap inputs
+       [(hooks/->Hook groups [accums/counter])]))
 
 (defn run-chash-single
   [inputs groups]
-  (run chash-store/factory map inputs groups))
+  (run chash-store/factory map inputs
+       [(hooks/->Hook groups [accums/stateful-counter])]))
 
 (defn run-chash-parallel
   [inputs groups]
-  (run chash-store/factory pmap inputs groups))
+  (run chash-store/factory pmap inputs
+       [(hooks/->Hook groups [accums/stateful-counter])]))
