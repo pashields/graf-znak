135dae7e50807c4a14432b9ed725b3bfdbc70358
Pat Shields <yeoldefortran@gmail.com>
2014-03-24 21:48:10 -0400

    Add travis yml

--- profiles.clj
+++ profiles.clj
@@ -3,6 +3,7 @@
                 *assert* true}
   :dependencies [[org.clojure/test.check "0.5.7"]
                  [criterium "0.4.3"]
-                 [org.clojure/data.csv "0.1.2"]]}
+                 [org.clojure/data.csv "0.1.2"]]
+  :plugins [[lein-typed "0.3.3"]]}
  :provided
  {:global-vars {*assert* false}}}
