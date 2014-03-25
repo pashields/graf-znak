d4d30c74ccb86cb6472d54c3a6ae4d44e33dcb95
Pat Shields <yeoldefortran@gmail.com>
2014-03-24 21:52:20 -0400

    Need lein-typed in project.clj for travis.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.typed "0.2.40"]]
+  :plugins [[lein-typed "0.3.3"]]
   :core.typed {:check [graf-znak.accumulators
                        graf-znak.hooks
                        graf-znak.atom-storage
